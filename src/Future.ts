a4cb8b94944e618fbb543bcc9050367ba93175f7
Frank-Mayer <git@frank-mayer.io>
2023-02-26 01:17:35 +0100

    Execute function in another thread

--- src/Future.ts
+++ src/Future.ts
@@ -1,3 +1,4 @@
+import { Panic } from "@frank-mayer/panic"
 import { Result } from "./Result"
 
 export class Future<T, E> extends Promise<Result<T, E>> {
@@ -63,6 +64,67 @@ export class Future<T, E> extends Promise<Result<T, E>> {
                     ok(values)
                 }
             })
+        })
+    }
+
+    /** @internal */
+    private static workerPool: Map<string, Worker> = new Map()
+
+    // eslint-disable-next-line max-lines-per-function
+    public static do<T>(fn: () => T): Future<T, Panic> {
+        // eslint-disable-next-line max-lines-per-function
+        return new Future((ok, err) => {
+            const str = fn.toString()
+
+            const worker = (Future.workerPool.has(str))
+                ? Future.workerPool.get(str) as Worker
+                : (() => {
+                    const objUrl =
+                        URL.createObjectURL(
+                            new File([
+                                new Blob(
+                                    [[
+                                        `const main = (${fn.toString()});`,
+                                        "self.onmessage = (async () => {",
+                                        "try {",
+                                        "postMessage({isOk: true, value: await main()});",
+                                        "} catch (e) {",
+                                        "postMessage({isOk: false, value: e});",
+                                        "}",
+                                        "})",
+                                    ].join("")], { type: "text/javascript" }
+                                )],
+                            `${fn.name}.js`,
+                            { type: "text/javascript" }
+                            ))
+                    // eslint-disable-next-line no-console
+                    console.debug(objUrl)
+                    const temp = new Worker(objUrl)
+                    Future.workerPool.set(str, temp)
+                    return temp
+                })()
+
+            try {
+                worker.onmessage = (event) => {
+                    const { isOk, value } = event.data as { isOk: boolean, value: T | Error }
+                    if (isOk) {
+                        ok(value as T)
+                    }
+                    else {
+                        err(new Panic((value as Error).message))
+                    }
+                }
+
+                worker.postMessage(null)
+            }
+            catch (e) {
+                if (e instanceof Error) {
+                    err(new Panic(e.message))
+                }
+                else {
+                    err(new Panic(JSON.stringify(e)))
+                }
+            }
         })
     }
 }
